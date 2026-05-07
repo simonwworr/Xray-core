e830be91460badc4167ba66755c39dbb901a3641
Simon Worr <simonwworr@users.noreply.github.com>
2026-05-07 08:26:29 +0800

    stats: add doc comment to newError helper function

--- common/stats/errors.generated.go
+++ common/stats/errors.generated.go
@@ -4,6 +4,7 @@ import "github.com/xtls/xray-core/common/errors"
 
 type errPathObjHolder struct{}
 
+// newError creates a new error with the stats package path context.
 func newError(values ...interface{}) *errors.Error {
 	return errors.New(values...).WithPathObj(errPathObjHolder{})
 }
